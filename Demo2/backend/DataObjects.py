6309526a5a7be9d3d0edbca0e3dae91946472165
ngohuy0972 <huyngoit99@gmail.com>
2020-12-15 07:56:16 +0700

    update

---
+++ Demo2/backend/DataObjects.py
@@ -0,0 +1,60 @@
+import psycopg2
+from BusinessObjects import Customer as CustomerEntity
+
+class Customer:
+    def __init__(self, ConnectionData):
+        self.ConnectionData = ConnectionData
+    def insert(self, customer):
+        con = None
+        try:
+            con = psycopg2.connect(user=self.ConnectionData['user'],
+                                  password=self.ConnectionData['password'],
+                                  host=self.ConnectionData['host'],
+                                  port=self.ConnectionData['port'],
+                                  database=self.ConnectionData['database'])
+            cur = con.cursor()
+            sql = "INSERT INTO TblCustomers(CustomerName, ContactName, Address, City, PostalCode, Country) VALUES (%s, %s, %s, %s, %s, %s)"
+            record_to_insert = (customer.CustomerName, customer.ContactName, customer.Address, customer.City, customer.PostalCode, customer.Country)
+            cur.execute(sql, record_to_insert)
+            con.commit()
+            con.close()
+            row = cur.fetchall()
+            result = []
+            for row in rows:
+                c = CustomerEntity()
+                c.fetch_data(row)
+                result.append(c.to_json())
+            return result
+        except (Exception, psycopg2.DatabaseError) as error:
+            return str(error)
+        finally:
+            if con is not None:
+                con.close()
+    def get_all(self):
+        con = None
+        try:
+            con = psycopg2.connect(user=self.ConnectionData['user'],
+                                  password=self.ConnectionData['password'],
+                                  host=self.ConnectionData['host'],
+                                  port=self.ConnectionData['port'],
+                                  database=self.ConnectionData['database'])
+            cur = con.cursor()
+            sql = "SELECT * FROM tblcustomers"
+            cur.execute(sql)
+            con.commit()
+            con.close()
+            row = cur.fetchall()
+            result = []
+            for row in rows:
+                c = CustomerEntity()
+                c.fetch_data(row)
+                result.append(c.to_json())
+            return result
+        except (Exception, psycopg2.DatabaseError) as error:
+            return str(error)
+        finally:
+            if con is not None:
+                con.close()
+
+if __name__ == "__main__":
+    print('this is data object package')

84914f9a2549702e6aef4f67187292aa37150060
ngohuy0972 <huyngoit99@gmail.com>
2020-12-22 18:52:46 +0700

    update 2nd

--- Demo2/backend/DataObjects.py
+++ Demo2/backend/DataObjects.py
@@ -6,86 +6,78 @@ class Customer:
     def __init__(self, ConnectionData):
         self.ConnectionData = ConnectionData
 
-    # def get_all_customer(self, customer):
-    #     con = None
-    #     try:
-    #         con = psycopg2.connect(user=self.ConnectionData['user'],
-    #                               password=self.ConnectionData['password'],
-    #                               host=self.ConnectionData['host'],
-    #                               port=self.ConnectionData['port'],
-    #                               database=self.ConnectionData['database'])
-    #         cur = con.cursor()
-    #         sql = "SELECT * FROM Customers"
-    #         cur.execute(sql)
-    #         con.commit()
-    #         con.close()
-    #         return sql.text
-    #     except (Exception, psycopg2.DatabaseError) as error:
-    #         return str(error)
-    #     finally:
-    #         if con is not None:
-    #             con.close()
+    def get_all_customer(self):
+        conn = psycopg2.connect(host = self.ConnectionData['host'],
+                            port = self.ConnectionData['port'],
+                            user = self.ConnectionData['user'],
+                            password = self.ConnectionData['password'],
+                            database = self.ConnectionData['database'])
+        cursor = conn.cursor()
+        sql = "SELECT * FROM Customers"
+        cursor.execute(sql)
+        conn.commit()
+        rows = cursor.fetchall()
+        result = []
+        for row in rows:
+            customer = CustomerEntity()
+            customer.fetch_data(row)
+            result.append(customer.to_json())
+        return result
 
     def insert_customer(self, customer):
-        con = None
+        conn = None
         try:
-            con = psycopg2.connect(user=self.ConnectionData['user'],
+            conn = psycopg2.connect(user=self.ConnectionData['user'],
                                   password=self.ConnectionData['password'],
                                   host=self.ConnectionData['host'],
                                   port=self.ConnectionData['port'],
                                   database=self.ConnectionData['database'])
-            cur = con.cursor()
+            cur = conn.cursor()
             sql = "INSERT INTO Customers(CustomerName, ContactName, Address, City, PostalCode, Country) VALUES (%s, %s, %s, %s, %s, %s)"
             record_to_insert = (customer.CustomerName, customer.ContactName, customer.Address, customer.City, customer.PostalCode, customer.Country)
             cur.execute(sql, record_to_insert)
-            con.commit()
-            con.close()
+            conn.commit()
+            conn.close()
             return 'Insert Customer successfully'
         except (Exception, psycopg2.DatabaseError) as error:
             return str(error)
         finally:
-            if con is not None:
-                con.close()
+            if conn is not None:
+                conn.close()
 
-    def delete_customer(self, customer):
-        con = None
-        try:
-            con = psycopg2.connect(user=self.ConnectionData['user'],
+    def delete_customer_by_id(self, customer):
+        conn = None
+        conn = psycopg2.connect(user=self.ConnectionData['user'],
                                   password=self.ConnectionData['password'],
                                   host=self.ConnectionData['host'],
                                   port=self.ConnectionData['port'],
                                   database=self.ConnectionData['database'])
-            cur = con.cursor()
-            sql = "DELETE FROM customers WHERE CustomerID = '2'"
-            cur.execute(sql)
-            con.commit()
-            con.close()
-            return 'Delete Customer successfully'
-        except (Exception, psycopg2.DatabaseError) as error:
-            return str(error)
-        finally:
-            if con is not None:
-                con.close()
+        cursor = conn.cursor()
+        sql = "DELETE FROM Customers WHERE CustomerID = %s"
+        cursor.execute(sql, (customer.CustomerID, ))
+        conn.commit()
+        n = cursor.rowcount
+        if n > 0:
+            return 'Deleted successfully', 200
+        return 'Id not found', 404        
 
     def update_customer(self, customer):
-        con = None
-        try:
-            con = psycopg2.connect(user=self.ConnectionData['user'],
+        conn = None
+        conn = psycopg2.connect(user=self.ConnectionData['user'],
                                   password=self.ConnectionData['password'],
                                   host=self.ConnectionData['host'],
                                   port=self.ConnectionData['port'],
                                   database=self.ConnectionData['database'])
-            cur = con.cursor()
-            sql = "UPDATE Customers SET CustomerName = 'HuyVanNgo' WHERE CustomerID = '6'"
-            cur.execute(sql)
-            con.commit()
-            con.close()
-            return 'Update Customer successfully'
-        except (Exception, psycopg2.DatabaseError) as error:
-            return str(error)
-        finally:
-            if con is not None:
-                con.close()
+        cursor = conn.cursor()
+        sql = """UPDATE Customers SET
+                    CustomerName=%s, ContactName=%s, Address=%s,
+                    City=%s, PostalCode=%s, Country=%s WHERE CustomerID=%s"""
+        cursor.execute(sql, (customer.CustomerName, customer.ContactName, customer.Address, customer.city, customer.PostalCode, customer.Country, customer.CustomerID))
+        conn.commit()
+        n = cursor.rowcount
+        if n > 0:
+            return 'Updated successfully', 200
+        return 'Id not found', 404
 
 
 if __name__ == "__main__":
--- Demo2/backend/app.py
+++ Demo2/backend/app.py
@@ -17,65 +17,58 @@ ConnectionData['database'] = 'northwind'
 def hello():    
     return 'this is backend'
 
-# Customer Start.
-@app.route('/get_all_customer', methods = ['GET'])
+@app.route('/customer/all')
 def get_all_customer():
-    c2 = do.Customer(ConnectionData)
-    s1 = c2.get_all_customer(1)
-    return s1
+    c = do.Customer(ConnectionData)
+    result = c.get_all()
+    return jsonify({
+        'data': result
+    })
 
-@app.route('/insert_customer')
+@app.route('/insert_customer', methods=['POST'])
 def insert_customer():
-    #ConnectionString = 'database=northwind user=postgres password=postgres host=10.0.2.15 port=5432'
-    c2 = do.Customer(ConnectionData)
-    c1 = bo.Customer(1, 'DAU xanh', 'Peter', '566 Nui Thanh', 'Danang', '50000', 'Vietnam')
-    s1 = c2.insert_customer(c1)
-    return s1
-    #return 'this is test insert'
-
-@app.route('/delete_customer')
-def delete_customer():
-    c2 = do.Customer(ConnectionData)
-    s1 = c2.delete_customer(1)
-    return s1
-
-@app.route('/update_customer')
-def update_customer():
-    c2 = do.Customer(ConnectionData)
-    s1 = c2.update_customer(1)
-    return s1
-
-@app.route('/user/all')
-def get_all_user():
-    result = do.Customer(ConnectionData).get_all()
-    return jsonify(result), 200
-    #return 'this is test user/all'
-
-@app.route('/test_send_receive', methods=['POST'])
-def test_send_receive():
-    x = request.json['x']
-    y = x + 1
-    result = {}
-    result['y'] = y
-    return jsonify(result), 200
-
-
-@app.route('/user/get_by_id', methods=['POST'])
-def user_get_by_id():
-    user_id = request.json['user_id']
-    result = {}
-    result['user_id'] = 1
-    return jsonify(result), 200
-
-@app.route('/user/insert', methods=['POST'])
-def user_insert():
     data = request.json
-    c1 = bo.Customer(data['CustomerID'], data['CustomerName'], data['ContactName'], data['Address'], data['City'], data['PostalCode'], data['Country'])
-    c2 = do.Customer(ConnectionData)
-    s1 = c2.insert(c1)
-    result = {}
-    result['message'] = s1
-    return jsonify(result), 200
+    customer = CustomerEntity(CustomerName=data['CustomerName'],
+                                ContactName=data['ContactName'],
+                                Address=data['Address'],
+                                City=data['City'],
+                                PostalCode=data['PostalCode'],
+                                Country=data['Country'])
+    c = Customer(connection_data)
+    message = c.insert(customer)
+    if message is None:
+        return jsonify({
+            'message': 'Cannot insert data'
+        }), 500
+    return jsonify({
+        'message': message
+    })
+
+@app.route('/customer/<int:id>', methods=['DELETE', 'PUT'])
+def delete_customer_by_id(id):
+    if request.method == 'DELETE':
+        # Delete user by id
+        customer = CustomerEntity(CustomerID=id)
+        c = Customer(ConnectionData)
+        result = c.delete_customer_by_id(customer)
+        return jsonify({
+            'message': result[0]
+        }), result[1]
+    else:
+        # Update user by id
+        data = request.json
+        customer = CustomerEntity(CustomerID=id,
+                                    CustomerName=data['CustomerName'],
+                                    ContactName=data['ContactName'],
+                                    Address=data['Address'],
+                                    City=data['City'],
+                                    PostalCode=data['PostalCode'],
+                                    Country=data['Country'])
+        c = Customer(ConnectionData)
+        result = c.update_customer(customer)
+        return jsonify({
+            'message': result[0]
+        }), result[1]
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
